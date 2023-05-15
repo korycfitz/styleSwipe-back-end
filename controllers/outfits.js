4529e8049042b0933a6e45042b02c4cde4e6a696
korycfitz <korycfitz@gmail.com>
2023-05-14 20:23:18 -0400

    stub up and code async create function for outfit model

---
+++ controllers/outfits.js
@@ -0,0 +1,24 @@
+import { Profile } from "../models/profile.js"
+import { Outfit } from "../models/outfit.js"
+
+async function create(req, res) {
+  try {
+    req.body.author = req.user.profile
+    const outfit = await Outfit.create(req.body)
+    const profile = await Profile.findByIdAndUpdate(
+      req.user.profile,
+      { $push: { outfits: outfit } },
+      { new: true }
+    )
+    outfit.author = profile
+    res.status(201).json(blog)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+
+export {
+  create,
+}
