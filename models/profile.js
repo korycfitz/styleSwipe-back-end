c05e5b55a097ea60b300c0c0f0c6d83520d971b5
mars-1002 <matthewabsalonramirez@gmail.com>
2023-05-15 21:20:13 -0400

    add new prop to profile

--- models/profile.js
+++ models/profile.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   photo: String,
-  outfits: [{ type: Schema.Types.ObjectId, ref: 'Outfit' }]
+  outfits: [{ type: Schema.Types.ObjectId, ref: 'Outfit' }],
+  mySwipes: [{ type: Schema.Types.ObjectId, ref: 'Swipe'}]
 },{
   timestamps: true,
 })
