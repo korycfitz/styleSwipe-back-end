ae78fe394e839039c3a55603440dfac8a8d661ce
korycfitz <korycfitz@gmail.com>
2023-05-17 21:30:42 -0400

    fix route for update

--- routes/outfits.js
+++ routes/outfits.js
@@ -2,7 +2,6 @@ import { Router } from 'express'
 import * as outfitsCtrl from '../controllers/outfits.js'
 import * as swipesCtrl from '../controllers/swipes.js'
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
-
 const router = Router()
 
 /*---------- Public Routes ----------*/
@@ -12,11 +11,10 @@ const router = Router()
 router.use(decodeUserFromToken)
 //index page will automatically redirect to '/:outfitId'
 router.get('/', checkAuth, outfitsCtrl.index) //works
-router.get('/:outfitId', checkAuth, outfitsCtrl.show) //works
+router.get('/', checkAuth, outfitsCtrl.show) //works
 router.post('/:outfitId/swipes', checkAuth, swipesCtrl.create)
 router.post('/new', checkAuth, outfitsCtrl.create) //works
-router.post('/:outfitId', checkAuth, outfitsCtrl.createComment) //works
-router.put('/:outfitId', checkAuth, outfitsCtrl.update) //works
+router.post('/', checkAuth, outfitsCtrl.createComment) //works
 router.delete('/:outfitId', checkAuth, outfitsCtrl.delete)
 router.delete('/:outfitId/:commentId', checkAuth, outfitsCtrl.deleteComment)
 
--- routes/profiles.js
+++ routes/profiles.js
@@ -16,6 +16,7 @@ router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
 //new routes
 router.get('/:userId', checkAuth, profilesCtrl.jumppage)
 router.get('/:userId/outfits', checkAuth, profilesCtrl.outfitIndex) // displays all user outfits
+router.put('/:userId/outfits/edit', checkAuth, outfitsCtrl.update) //works
 router.get('/:userId/outfits/:outfitId', checkAuth, profilesCtrl.outfitShow) //play 1 user ouitfit
 router.get('/:userId/swipes', checkAuth, profilesCtrl.swipeIndex) // display all user swipes
 router.get('/:userId/swipes/:swipeId', checkAuth, profilesCtrl.swipeShow) //display 1 user swipe
