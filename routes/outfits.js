001fdd67a853d7cb3f92394bf9de53cb908fcd57
korycfitz <korycfitz@gmail.com>
2023-05-19 01:14:21 -0400

    remove commented out code

--- routes/outfits.js
+++ routes/outfits.js
@@ -10,11 +10,11 @@ const router = Router()
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.get('/', checkAuth, outfitsCtrl.index) //works
-router.get('/:outfitId', checkAuth, outfitsCtrl.show) //works
-router.post('/', checkAuth, outfitsCtrl.create) //works
-router.post('/:outfitId/comments', checkAuth, outfitsCtrl.createComment) //works
-router.put('/:outfitId', checkAuth, outfitsCtrl.update) //works
+router.get('/', checkAuth, outfitsCtrl.index)
+router.get('/:outfitId', checkAuth, outfitsCtrl.show) 
+router.post('/', checkAuth, outfitsCtrl.create) 
+router.post('/:outfitId/comments', checkAuth, outfitsCtrl.createComment) 
+router.put('/:outfitId', checkAuth, outfitsCtrl.update) 
 router.delete('/:outfitId', checkAuth, outfitsCtrl.delete)
 //will be implementing when swipe funcitionality is implemented on front end 
 // router.post('/:outfitId/swipes', checkAuth, swipesCtrl.create)
